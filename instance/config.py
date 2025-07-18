0c5b027c902cc8e3a824c35a90a91fdef31d4b5b
alpha <godspowerlawrence008@gmail.com>
2025-07-18 12:04:08 -0400

    adjusted UI

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,3 @@
 SECRET_KEY="oEAP6ceI8t0-wSXpG7f8ltD6gY05PGOtSwm8AOHC"
-SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://root:root@127.0.0.1:8889/admerce_db"
+# SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://root:root@127.0.0.1:8889/admerce_db"
+SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://admerceconnect:ikechi100@admerceconnect.mysql.pythonanywhere-services.com/admerceconnect$default"
